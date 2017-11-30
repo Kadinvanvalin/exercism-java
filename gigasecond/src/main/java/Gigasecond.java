2e03768aa93979276ac64d58c9fe92c5fff847f4
kadinvanvalin <kadinvanvalin@yahoo.com>
2017-11-30 10:54:48 -0500

    add gigasecond constructors

--- gigasecond/src/main/java/Gigasecond.java
+++ gigasecond/src/main/java/Gigasecond.java
@@ -1,18 +1,40 @@
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
 
+/**
+ * @author kadinvanvalin
+ * Calculate the moment when someone has lived for 10^9 seconds.
+ */
 class Gigasecond {
+    private LocalDateTime birthday;
+    private LocalDateTime gigasecondOld;
+    private long gigasecond = 1_000_000_000;
 
+    /**
+     *
+     * @param birthDate birthdate of the person you are checking
+     */
     Gigasecond(LocalDate birthDate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.birthday =  LocalDateTime.of(birthDate, LocalTime.MIDNIGHT);
+        this.gigasecondOld = birthday.plusSeconds(gigasecond);
     }
 
+
+    /**
+     *
+     * @param birthDateTime birthdate of the person you are checking
+     */
     Gigasecond(LocalDateTime birthDateTime) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.birthday = birthDateTime;
+        this.gigasecondOld = birthday.plusSeconds(gigasecond);
+
     }
 
+    /**
+     * 
+     * @return the time when user is a gigasecond old
+     */
     LocalDateTime getDate() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return gigasecondOld;
     }
 
 }
